06fbd1b8691086fadf699a180e7d8e4f7c488cab
Michael Walker <mike@barrucadu.co.uk>
2017-10-05 20:10:08 +0100

    Define basic types and primops

--- Test/MiniFu.hs
+++ Test/MiniFu.hs
@@ -1,4 +1,45 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Test.MiniFu where
 
-minifu :: String
-minifu = "Hello, world!"
+import qualified Control.Concurrent.Classy as C
+import Data.List.NonEmpty (NonEmpty(..))
+import qualified Control.Monad.Cont as K
+
+-- | Threads are just identified by their creation order.
+newtype ThreadId = ThreadId Int
+  deriving (Eq, Ord)
+
+-- | A scheduler is a stateful function which chooses a thread to run.
+type Scheduler s = NonEmpty ThreadId -> s -> (ThreadId, s)
+
+-- | A MiniFu computation is just a continuation over primops.
+newtype MiniFu m a = MiniFu { runMiniFu :: K.Cont (PrimOp m) a }
+  deriving (Functor, Applicative, Monad)
+
+-- | One of the basic actions that a @MonadConc@ can do.
+data PrimOp m where
+  Fork         :: MiniFu m () -> (ThreadId -> PrimOp m) -> PrimOp m
+  NewEmptyMVar :: (MVar m a -> PrimOp m)                -> PrimOp m
+  PutMVar      :: MVar m a -> a -> PrimOp m             -> PrimOp m
+  TakeMVar     :: MVar m a -> (a -> PrimOp m)           -> PrimOp m
+  Stop         :: m ()                                  -> PrimOp m
+
+-- | @MVar@s have a unique ID too, used in thread blocking.
+newtype MVarId = MVarId Int
+  deriving (Eq, Ord)
+
+-- | An @MVar@ is a @CRef@ in the underlying monad, holding a maybe
+-- value, with a unique identifier.
+data MVar m a = MVar
+  { mvarId  :: MVarId
+  , mvarRef :: C.CRef m (Maybe a)
+  }
+
+-------------------------------------------------------------------------------
+
+-- | Execute a concurrent computation with a given scheduler, and
+-- return the result.
+minifu :: C.MonadConc m => Scheduler s -> s -> MiniFu m a -> m (Maybe a, s)
+minifu sched s (MiniFu ma) = undefined
